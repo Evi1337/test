d0194efc0d5877f760ee2cde104550456bb5b77b
Evi1337 <92557529+Evi1337@users.noreply.github.com>
2021-10-15 04:52:48 +0300

    Add files via upload

--- load.c
+++ load.c
@@ -1,17 +1,30 @@
 #include "load.h"
 #include "ghj.h"
 #include <stdio.h>
+#ifdef WIN32
+	#include <windows.h>
+#else
+	#include <dlfcn.h>
+#endif
 void LoadRun(const char * const s)
 {
 	void * lib;
 	void (*fun)(void);
-    lib = LoadLibrary(s);
+ 	#ifdef WIN32
+ 		lib = LoadLibrary(s);
+ 	#else
+		lib = dlopen(s, RTLD_LAZY);
+	#endif
  	if (!lib)
 	{
 		printf("cannot open library '%s'\n", s);
 	 	return;
 	}
-	fun = (void (*)(void))GetProcAddress((HINSTANCE)lib, "mesim");
+	#ifdef WIN32
+		fun = (void (*)(void))GetProcAddress((HINSTANCE)lib, "mesim");
+	#else
+		fun = (void (*)(void))dlsym(lib, "run");
+	#endif
 	if (fun == NULL)
 	{
 		printf("cannot load function mesim\n", s);
@@ -20,5 +33,9 @@ void LoadRun(const char * const s)
 	{
 		fun();
 	}
-	FreeLibrary((HINSTANCE)lib);
+	#ifdef WIN32
+		FreeLibrary((HINSTANCE)lib);
+	#else
+		dlclose(lib);
+	#endif
 }
